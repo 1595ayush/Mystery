b21b134334e1a3970168381765080a1189a530a4
Ayush Shah <1595ayush@gmail.com>
2018-08-03 09:20:13 +0530

    Code changes.. more coming

--- main.py
+++ main.py
@@ -3,41 +3,58 @@ import urllib.request, json
 import datetime
 
 url_link = "http://www.moneycontrol.com/techmvc/html/earnings/earnings-calender.json"
+header = "Stock,MeetingDate\n"
+main_csv = "" + header
 
-with urllib.request.urlopen(url_link) as url:
-    data = json.loads(url.read().decode())
-    print(data)
-main_csv = "Stock,MeetingDate\n"
-for entry in data:
-    result_date = entry['date']
-    compare_date = datetime.datetime.strptime(result_date, '%Y-%m-%d').strftime('%d %b %Y')
-    soup = BeautifulSoup(entry['issue'])
+
+def get_earnings_json():
+    with urllib.request.urlopen(url_link) as earnings_urlopen:
+        data = json.loads(earnings_urlopen.read().decode())
+        print(data)
+        return data
+
+
+earnings_data = get_earnings_json()
+
+for entry in earnings_data:
+
+    soup = BeautifulSoup(entry['issue'], 'html.parser')
+
+    result_date = datetime.datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%d %b %Y')
     stock = soup.find("div", {"class": "Ohidden"}).text
-    stock_short = soup.find("a")["href"]
-    href_details = stock_short.split('/')[7]
-    if(href_details == 'KMB'):
-        href_details  = "kmf"
-    link = "http://www.moneycontrol.com/tech_charts/bse/his/" + href_details.lower() + ".csv"
-    #print(link)
+    href_link = soup.find("a")["href"]
+    stock_short_name = href_link.split('/')[7]
 
-    if(href_details == "TCS"):
-        with urllib.request.urlopen(link) as u:
-            csv = ""
-            print(u)
-            data1 = u.read().decode().split("\n")
-            count = -1;
-            for x in range(len(data1) - 1, 0, -1):
-                row = data1[x].split(",")
-                rec_date = row[0]
-                if (count >= 0 and count <= 5):
+    if stock_short_name == 'KMB':
+        stock_short_name = "kmf"
+    exchange = "bse"
+    price_url = "http://www.moneycontrol.com/tech_charts/{exchange}/his/{name}.csv".format(exchange=exchange, name=stock_short_name.lower())
+
+    if stock_short_name == "TCS":
+        with urllib.request.urlopen(price_url) as price_urlopen:
+            line = ""
+            count = -1
+
+            price_data_list = price_urlopen.read().decode().split("\n")
+
+            for x in range(len(price_data_list) - 1, 0, -1):
+
+                row = price_data_list[x].split(",")
+
+                price_date = row[0]
+
+                if 0 <= count <= 5:
                     count += 1
-                    csv += rec_date + "," + row[4] + ","
-                if(compare_date == rec_date):
+                    close_price = row[4]
+                    line += price_date + "," + close_price + ","
+
+                if result_date == price_date:
                     count = 0
-                close_price = row[4]
-            main_csv += stock + "," + compare_date + "," + csv + "\n"
+
+            main_csv += stock + "," + result_date + "," + line + "\n"
     with open("abc.csv", "w") as m:
         m.write(main_csv)
-    print(href_details)
-    print(stock)
-    print(result_date)
+
+    #print(href_details)
+    #print(stock)
+    #print(result_date)
